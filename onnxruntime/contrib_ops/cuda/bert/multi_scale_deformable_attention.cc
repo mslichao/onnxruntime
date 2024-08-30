6a17efe897fe61ba9074012fc911e2a7203a5297
Qingnan Duan <duanqn@proton.me>
2024-08-30 08:48:56 +0000

    CUDA ops should inherit from CudaKernel

--- onnxruntime/contrib_ops/cuda/bert/multi_scale_deformable_attention.cc
+++ onnxruntime/contrib_ops/cuda/bert/multi_scale_deformable_attention.cc
@@ -6,22 +6,27 @@
 #include "core/framework/op_kernel.h"
 
 namespace onnxruntime::contrib::cuda{
-  void MultiScaleDeformableAttention::ComputeCUDA(
-    const float* value,
-    const int64_t* value_spatial_shapes,
-    const float* reference_points,
-    const float* sampling_locations,
-    const float* attention_weights,
-    float* output,
-    int64_t M,
-    int64_t L,
-    int64_t P,
-    int64_t D,
-    int64_t Q,
-    onnxruntime::concurrency::ThreadPool* thread_pool,
-    onnxruntime::AllocatorPtr alloc) const {
-    memset(output, 0, Q * M * D * sizeof(float));
-    return;
+  Status MultiScaleDeformableAttention::ComputeInternal(OpKernelContext* context) const {
+    const auto* value = context->Input<Tensor>(0);  // Shape: [1, S, M, D]
+    const auto* value_spatial_shapes = context->Input<Tensor>(1); // Shape: [L, 2]
+    const auto* reference_points = context->Input<Tensor>(2); // Shape: [1, L, Q, 2]
+    const auto* sampling_locations = context->Input<Tensor>(3); // Shape: [1, L, Q, M, P, 2]
+    const auto* attention_weights = context->Input<Tensor>(4);  // Shape: [1, L, Q, M, P]
+
+    const auto& value_input_shape = value->Shape();
+    const auto& value_spatial_shapes_input_shape = value_spatial_shapes->Shape();
+    const auto& attention_weights_input_shape = attention_weights->Shape();
+
+    const int64_t M = value_input_shape[2];
+    const int64_t D = value_input_shape[3];
+    const int64_t L = value_spatial_shapes_input_shape[0];
+    const int64_t P = attention_weights_input_shape[4];
+    const int64_t Q = attention_weights_input_shape[2];
+
+    auto* output = context->Output(0, { 1, Q, M*D }); // Shape: [1, Q, M*D]
+    float * output_ptr = output->MutableData<float>();
+    // memset(output_ptr, 0, Q * M * D * sizeof(float));  // qiduan: cannot use memset on GPU memory
+    return Status::OK();
   }
 
   ONNX_OPERATOR_KERNEL_EX(
@@ -29,7 +34,7 @@ namespace onnxruntime::contrib::cuda{
     kMSDomain,
     1,
     kCudaExecutionProvider,
-    KernelDefBuilder()
+    (*KernelDefBuilder::Create())
     .TypeConstraint(
       "T1",
       {DataTypeImpl::GetTensorType<float>()})
--- onnxruntime/contrib_ops/cuda/bert/multi_scale_deformable_attention.h
+++ onnxruntime/contrib_ops/cuda/bert/multi_scale_deformable_attention.h
@@ -3,29 +3,17 @@
 
 #pragma once
 
-#include "contrib_ops/cpu/attnlstm/multi_scale_deformable_attention.h"
+#include "core/common/common.h"
+#include "core/providers/cuda/cuda_kernel.h"
+
+using onnxruntime::cuda::CudaKernel;
 
 namespace onnxruntime::contrib::cuda {
-  class MultiScaleDeformableAttention final : public onnxruntime::contrib::MultiScaleDeformableAttentionBase {
+  class MultiScaleDeformableAttention final : public CudaKernel{
     public:
-    MultiScaleDeformableAttention(const onnxruntime::OpKernelInfo& info): onnxruntime::contrib::MultiScaleDeformableAttentionBase(info) {
-      route = ImplementationRoute::CUDA;
-    }
+    MultiScaleDeformableAttention(const onnxruntime::OpKernelInfo& info): CudaKernel(info) {}
     ~MultiScaleDeformableAttention() = default;
     protected:
-    void ComputeCUDA(
-      const float* value,
-      const int64_t* value_spatial_shapes,
-      const float* reference_points,
-      const float* sampling_locations,
-      const float* attention_weights,
-      float* output,
-      int64_t M,
-      int64_t L,
-      int64_t P,
-      int64_t D,
-      int64_t Q,
-      onnxruntime::concurrency::ThreadPool* thread_pool,
-      onnxruntime::AllocatorPtr alloc) const override;
+    Status ComputeInternal(OpKernelContext* context) const override;
   };
 }   // namespace onnxruntime::contrib::cuda
